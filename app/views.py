0ff83864a21b33f97bcdf90d8398b0b73ceb3afb
srj92 <suraj.gupta5077@gmail.com>
2021-09-29 16:38:24 +0800

    third version

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from app import app
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, make_response
+import jwt
+from functools import wraps
 
 @app.route("/")
 def index():
@@ -9,3 +11,18 @@ def index():
 def trigger():
     print('Akshi Bhasker developed this code/ Girl Engineers')
     return 'Navtech Secured Script Flask - Test Version 1 '
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = 6194df3c829666a24686dbc492081659408b9cafc18af2f19136e70ecb9cb871
+        # jwt is passed in the request header
+        if token in request.headers:
+            token = request.headers['token']
+        # return 401 if token is not passed
+        if not token:
+            return jsonify({'message' : 'Token is missing !!'}), 401
+
+@app.route("/test")
+def test():
+    return 'Navtech API running... '
