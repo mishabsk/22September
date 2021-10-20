bc4113a3d84e227a7e655e8e0fddaab6e2524236
mishabsk <akshita.misha.bhasker@gmail.com>
2021-10-20 18:19:44 +0000

    API Submission

--- flask_website/app/views.py
+++ flask_website/app/views.py
@@ -1,10 +1,39 @@
 from app import app
-from flask import Flask, render_template, request, jsonify, make_response
+from flask import Flask, render_template,redirect,request, jsonify, make_response
+import glob
+import json
 from functools import wraps
+import sys
+sys.path.insert(1, '/root/jFiles')
+import ver
+import os
+
+#import sys
+# insert at 1, 0 is the script path (or '' in REPL)
+#sys.path.insert(1,'/root/jFiles')
+#import ver
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/upload")
+def upload():
+    return render_template('upload.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        password = request.form.get('psw')
+        if password != '782701794ad0eae0957c0d35fb32cf5d675d38566b76838b6dbcb9ecdf8fe957':
+            return "Unauthorised"
+        if 'files[]' not in request.files:
+            return redirect(request.url)
+        files = request.files.getlist('files[]')
+        for file in files:
+            filename = file.filename
+            file.save(os.path.join("/root/jFiles", filename))
+    return "happy upload!"
 
 @app.route("/trigger")
 def trigger():
@@ -22,7 +51,8 @@ def API():
     headers = request.headers
     auth = headers.get("X-Api-Key")
     if auth == '782701794ad0eae0957c0d35fb32cf5d675d38566b76838b6dbcb9ecdf8fe957':
-        return jsonify({"message": "OK: Authorized"}), 200
+        json_dump= ver.get_loc()
+        return jsonify({"Output":json_dump}), 200
     else:
         return jsonify({"message": "ERROR: Unauthorized"}), 401
 
